875626c903015771b32356fde70014395b215f16
Matt Loberg <loberg.matt@gmail.com>
2020-08-18 20:34:13 -0500

    fix copy functionality
    fixes #6

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -1,34 +1,35 @@
-import "alpinejs";
-import lunr from "lunr";
-import { shuffle } from "lodash";
+import 'alpinejs';
+
+import { shuffle } from 'lodash';
+import lunr from 'lunr';
 
 window.gifs = () => {
     return {
-        query: "",
+        query: '',
         images: [],
         index: null,
         results: [],
         toast: false,
         async init() {
-            const resp = await fetch("/gifs.json");
+            const resp = await fetch('/gifs.json');
             const data = await resp.json();
-            const gifs = data.map(gif => {
+            const gifs = data.map((gif) => {
                 gif.file = new URL(gif.file, window.location.href).toString();
                 gif.frame = new URL(gif.frame, window.location.href).toString();
                 return gif;
             });
 
             this.images = gifs;
-            this.index = lunr(function() {
+            this.index = lunr(function () {
                 this.b(0);
-                this.pipeline.remove(lunr.stopWordFilter)
+                this.pipeline.remove(lunr.stopWordFilter);
 
-                this.ref("file");
-                this.field("name", { boost: 10 });
-                this.field("tags");
-                this.field("category", { boost: 5 });
+                this.ref('file');
+                this.field('name', { boost: 10 });
+                this.field('tags');
+                this.field('category', { boost: 5 });
 
-                gifs.forEach(img => this.add(img));
+                gifs.forEach((img) => this.add(img));
             });
 
             // load some random gifs
@@ -36,13 +37,13 @@ window.gifs = () => {
         },
         loadGif(event) {
             const target = event.target;
-            if (target.src.endsWith("gif")) {
+            if (target.src.endsWith('gif')) {
                 return;
             }
 
-            const src = target.getAttribute("data-src");
+            const src = target.getAttribute('data-src');
             const img = new Image();
-            img.onload = function() {
+            img.onload = function () {
                 target.src = this.src;
             };
             img.src = src;
@@ -53,20 +54,52 @@ window.gifs = () => {
                 return;
             }
 
-            this.results = this.index.search(this.query).map(result => {
-                return this.images.filter(img => img.file === result.ref)[0];
+            this.results = this.index.search(this.query).map((result) => {
+                return this.images.filter((img) => img.file === result.ref)[0];
             });
         },
-        copy(event) {
-            const input = event.target.querySelector("input") || event.target.parentNode.querySelector("input");
-            input.select();
-            if (document.execCommand("copy")) {
-                this.toast = true;
-                setTimeout(() => this.toast = false, 5000);
-                return;
+        async copy(file, event) {
+            this.copyToClipboard(file)
+                .catch(async () => {
+                    this.copyToClipboardFallback(file);
+                })
+                .then(() => {
+                    this.toast = true;
+                    setTimeout(() => (this.toast = false), 5000);
+                })
+                .catch(() => {
+                    const input =
+                        event.target.querySelector('input') ||
+                        event.target.parentNode.querySelector('input');
+
+                    input.removeAttribute('disabled');
+                    input.focus();
+                    input.select();
+                });
+        },
+        async copyToClipboard(text) {
+            if (navigator.clipboard) {
+                return await navigator.clipboard.writeText(text);
             }
-            input.removeAttribute("disabled");
-            input.select();
+
+            throw new Error('fallback');
+        },
+        copyToClipboardFallback(text) {
+            const textArea = document.createElement('textarea');
+            textArea.value = text;
+            textArea.style.top = '0';
+            textArea.style.left = '0';
+            textArea.style.position = 'fixed';
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+
+            const success = document.execCommand('copy');
+            document.body.removeChild(textArea);
+
+            if (!success) {
+                throw new Error('cannot copy');
+            }
         },
     };
 };
